af4203af405e2f3367d337e59cd1599e4f7c0566
Samyush <samyushmaharjan@gmail.com>
2020-12-01 20:50:50 +0545

    added comments only last changes for today

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--the above used for image display and the HomeWorld is the component -->
+  <h1>My Shop</h1>
+    <p class="animated fadeInRight">Take a tour at our offerings</p>
+  <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+// import HelloWorld from './components/HelloWorld.vue'
+//the above import helps on importing component upon components/HelloWorld.vue
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  FontAwesomeIcon
   }
 }
 </script>
